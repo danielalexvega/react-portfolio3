485c126aff5b1295629050ff5d317cfbca0c3e64
Daniel Vega <danielalexvega@gmail.com>
2020-09-03 09:56:43 -0500

    Updates

---
+++ precache-manifest.5cf0f19d017e37e9734cb902d6c9523d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaf8648570e8bbb8d36ce8f516cfd33",
+    "url": "/react-portfolio3/index.html"
+  },
+  {
+    "revision": "a899d881d0d93d160a27",
+    "url": "/react-portfolio3/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5bcd22b674708af59ee2",
+    "url": "/react-portfolio3/static/css/main.3937c916.chunk.css"
+  },
+  {
+    "revision": "a899d881d0d93d160a27",
+    "url": "/react-portfolio3/static/js/2.b9940970.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio3/static/js/2.b9940970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcd22b674708af59ee2",
+    "url": "/react-portfolio3/static/js/main.2c86bf1b.chunk.js"
+  },
+  {
+    "revision": "36d2b8d002c14663a819",
+    "url": "/react-portfolio3/static/js/runtime-main.aebab458.js"
+  },
+  {
+    "revision": "ba144dbce63a820c94e6bc96025265a7",
+    "url": "/react-portfolio3/static/media/dnd-website-portfolio.ba144dbc.png"
+  },
+  {
+    "revision": "83fd6967f0b21b8b68cb82f3d9c9836e",
+    "url": "/react-portfolio3/static/media/express-note-taker.83fd6967.png"
+  },
+  {
+    "revision": "b88b250c10139ca66fb1af666c345023",
+    "url": "/react-portfolio3/static/media/me-300x500.b88b250c.png"
+  },
+  {
+    "revision": "58f4a7c4a5001f841310c35efa0578af",
+    "url": "/react-portfolio3/static/media/prepoll-website-port.58f4a7c4.png"
+  },
+  {
+    "revision": "15920979b2122c5c62356979d46a211d",
+    "url": "/react-portfolio3/static/media/tigerbanner2000x200.15920979.png"
+  }
+]);
